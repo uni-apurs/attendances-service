d6ee4ff0e6f143f6b74c1011ab8d22e8a84ca699
Stefan Katic <katicstefan.coder@gmail.com>
2021-07-23 20:47:28 +0200

    first commit

---
+++ src/main/java/com/apurs/microservices/attendancesservice/AttendancesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apurs.microservices.attendancesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendancesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendancesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apurs/microservices/attendancesservice/AttendancesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apurs.microservices.attendancesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendancesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
